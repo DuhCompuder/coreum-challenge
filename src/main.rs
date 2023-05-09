9d15eb49f761f683a9c35ce0f3e3202dbe56fe80
Milad Zahedi <mza1989@gmail.com>
2023-05-09 11:33:25 +0330

    fixed comments and added more examples

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ fn main() {
     println!("Hello, Coreum!");
 }
 
-// A user can submit a `MultiSend` transaction to transfer multiple coins (denoms) from multiple input
-// addresses to multiple output addresses. A denom is the name or symbol for a coin type, e.g USDT and
-// USDC can be considered different denoms; in cosmos ecosystem they are called denoms,
-// in ethereum world they are called symbols.
+// A user can submit a `MultiSend` transaction (similar to bank.MultiSend in cosmos sdk) to transfer multiple
+// coins (denoms) from multiple input addresses to multiple output addresses. A denom is the name or symbol
+// for a coin type, e.g USDT and USDC can be considered different denoms; in cosmos ecosystem they are called
+// denoms, in ethereum world they are called symbols.
 // The sum of input coins and output coins must match for every transaction.
 struct MultiSend {
     // inputs contain the list of accounts that want to send coins from, and how many coins from each account we want to send.
@@ -36,7 +36,7 @@ struct DenomDefinition {
     // burn_rate is a number between 0 and 1. If it is above zero, in every transfer,
     // some additional tokens will be burnt on top of the transferred value, from the senders address.
     // The tokens to be burnt are calculated by multiplying the TransferAmount by burn rate, and
-    // rounding it up to an integer value. for example if an account sends 100 token and burn_rate is
+    // rounding it up to an integer value. For example if an account sends 100 token and burn_rate is
     // 0.2, then 120 (100 + 100 * 0.2) will be deducted from sender account and 100 will be deposited to the recipient
     // account (i.e 20 tokens will be burnt)
     burn_rate: f64,
@@ -48,12 +48,35 @@ struct DenomDefinition {
 // Implement `calculate_balance_changes` with the following requirements.
 // - Output of the function is the balance changes that must be applied to different accounts
 //   (negative means deduction, positive means addition), or an error. the error indicates that the transaction must be rejected.
-// - If sum of inputs and outputs in multi_send_tx does match the tx must be rejected(i.e return error).
+// - If sum of inputs and outputs in multi_send_tx does not match the tx must be rejected(i.e return error).
 // - Apply burn_rate and commission_rate as described by their definition.
 // - If the sender does not have enough balances (in the original_balances) to cover the input amount on top of burn_rate and
 // commission_rate, the transaction must be rejected.
+// - burn_rate and commission_rate does not apply to the issuer. So to calculate the correct values you must do this for every denom:
+//      - sum all the inputs coming from accounts that are not an issuer (let's call it non_issuer_input_sum)
+//      - sum all the outputs going to accounts that are not an issuer (let's call it non_issuer_output_sum)
+//      - total burn amount is total_burn = min(non_issuer_input_sum, non_issuer_output_sum)
+//      - total_burn is distributed between all input accounts as: account_share = roundup(total_burn * input_from_account / non_issuer_input_sum)
+//      - total_burn_amount = sum (account_shares) // notice that in previous step we rounded up, so we need to recalculate the total again.
+//      - commission_rate is exactly the same, but we send the calculate value to issuer, and not burn.
+//      - Example:
+//          burn_rate: 10%
+//
+//          inputs:
+//          60, 90
+//          25 <-- issuer
+//
+//          outputs:
+//          50
+//          100 <-- issuer
+//          25
+//          In this case burn amount is: min(non_issuer_inputs, non_issuer_outputs) = min(75+75, 50+25) = 75
+//          Expected burn: 75 * 10% = 7.5
+//          And now we divide it proportionally between all input sender: first_sender_share  = 7.5 * 60 / 150  = 3
+//                                                                        second_sender_share = 7.5 * 90 / 150  = 4.5
+// - In README.md we have provided more examples to help you better understand the requirements.
 // - Write different unit tests to cover all the edge cases, we would like to see how you structure your tests.
-// - Try to improve the error return type of the function (currently String).
+//   There are examples in README.md, you can convert them into tests, but you should add more cases.
 fn calculate_balance_changes(
     original_balances: Vec<Balance>,
     definitions: Vec<DenomDefinition>,
